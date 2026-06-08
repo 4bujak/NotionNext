0c445a61ae64d84c11f8781fbc6cabce2f304e6f
4bujak <84439760+4bujak@users.noreply.github.com>
2026-06-08 15:06:50 +0900

    Update style.js
    불필요 설정 제거

--- themes/medium/style.js
+++ themes/medium/style.js
@@ -11,28 +11,5 @@ const Style = () => {
     .dark body{
         background-color: black;
     }
-/* 한글 폰트 Pretendard 불러오기 */
-  @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardv
-  ariable-dynamic-subset.css");
-
-  /* 1) 폰트 전체 적용 */
-  body, button, input, textarea, select, .notion, #theme-medium {
-    font-family: "Pretendard Variable", Pretendard, -apple-system,
-      system-ui, "Apple SD Gothic Neo", "Noto Sans KR", sans-serif !important;
-  }
-
-  /* 2) 본문 읽기 너비 좁혀서 가독성 ↑ (Medium 느낌) */
-  #article-wrapper {
-    max-width: 720px !important;
-    line-height: 1.8;
-    padding-bottom: 3rem;
-  }
-
-  /* 3) 목록(홈) 영역 너비 */
-  #container-inner {
-    max-width: 1080px !important;
-  }
-  `}</style>
-}
 
 export { Style }
